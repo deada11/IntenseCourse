acfeb1bed3ef3df8e7fdb5d192d27b68dabdd5cb
deada11 <deada11@gmail.com>
2021-12-22 22:48:24 +0300

    Обработка ошибок на стороне клиента. Добавили сохранение и загрузку сообщений в файл

--- server.py
+++ server.py
@@ -1,33 +1,51 @@
 from flask import Flask, request, render_template
 import time  # подключить модуль "time"
 from datetime import datetime  # подключаем модуль datetime и из него берем него класс datetime (одноименный)
+import json
+
+messages_file = "./data/messages.json"
+json_file = open(messages_file, "rb")  # Пока что только читаем файл
+data = json.load(json_file)
+if "all_messages" not in data:
+    print(f"Can't find the key 'all_messages' in {messages_file}")
+    exit(1)
+
+all_messages = data["all_messages"]  # Загружаем данные из файла по ключу и присваиваем переменной
+
+
+def save_messages():
+    messages = {
+        "all_messages": all_messages
+    }
+    json_file_messages = open(messages_file, "w")  # Открываем файл для записи
+    json.dump(messages, json_file_messages)  # Пишем структуру в файл
+
 
 app = Flask(__name__)
 
 
 def time_format(t):
-    return datetime.fromtimestamp(t)
+    return str(datetime.fromtimestamp(t))
 
 
-all_messages = [  # В этом списке хранятся все сообщения чата
-    {  # каждое сообщение это словарь с полями text, name и time
-        "text": "test message",
-        "name": "Mike",
-        "time": time_format(time.time())
-    },
-    {
-        "text": "lorem redeemer",
-        "name": "Paul",
-        "time": time_format(time.time())
-    },
-    {
-        "text": "tester pester",
-        "name": "Tester",
-        "time": time_format(time.time())
-    }
-
-]
-
+# all_messages = [  # В этом списке хранятся все сообщения чата
+#     {  # каждое сообщение это словарь с полями text, name и time
+#         "text": "test message",
+#         "name": "Mike",
+#         "time": time_format(time.time())
+#     },
+#     {
+#         "text": "lorem redeemer",
+#         "name": "Paul",
+#         "time": time_format(time.time())
+#     },
+#     {
+#         "text": "tester pester",
+#         "name": "Tester",
+#         "time": time_format(time.time())
+#     }
+#
+# ]
 
 @app.route("/chat")
 def chat():
@@ -51,16 +69,12 @@ def send_message():
     text = request.args['text']
 
     # Проверки на длину имени
-    if len(name) < 3:
-        return "Имя пользователя не может быть короче 3 символов"
-    elif len(name) > 100:
-        return "Имя пользователя не может быть длиннее 100 символов"
+    if len(name) < 3 or len(name) > 100:
+        return "ERROR"
 
     # Проверки на длину сообщения
-    if len(text) < 1:
-        return "Текст сообщения не может быть пустым"
-    elif len(text) > 3000:
-        return "Текст сообщения не может превышать 3000 символов"
+    if len(text) < 1 or len(text) > 3000:
+        return "ERROR"
 
     message = {
         "text": text,
@@ -69,7 +83,8 @@ def send_message():
     }
 
     all_messages.append(message)
+    save_messages()
     return "OK"
 
 
-app.run()
+app.run(host="0.0.0.0", port=80)
